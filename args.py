915681976eacb2a7b2f15406090e3100e3ea6364
Harrison Inocencio <hain222@uky.edu>
2018-07-18 10:56:48 -0400

    Add trim option, set up __mass_trim function

--- args.py
+++ args.py
@@ -23,10 +23,12 @@ import argparse
 
 # Output sub dir names (terminal '/' required)
 merge_dir = 'merged_reads/'
+trim_dir = 'trimmed/'
 
 # parser HELPS
 read_dir_help = "read_dir HELP"
 output_dir_help = "output_dir HELP"
+trim_args_help = "trim_args HELP"
 
 # parse func
 # parses arguments from the command line
@@ -34,6 +36,7 @@ def parse():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('read_dir', help=read_dir_help)
 	parser.add_argument('output_dir', help=output_dir_help)
+	parser.add_argument('-t', '--trim_args', help=trim_args_help)
 	args = parser.parse_args()
 
 	return args
