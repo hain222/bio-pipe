8edfd8460f3229f95f20732e6cbd97116f9e96f0
Harrison Inocencio <hain222@uky.edu>
2018-07-18 13:07:09 -0400

    Add config class

---
+++ config/cargs.py
@@ -0,0 +1,33 @@
+# File: cargs.py
+# Author: Harrison Inocencio
+# Date: 07-18-18
+# Purpose: Contains consts/arg parsing for the config class and the 
+#		   configQuery script
+
+# Notes:
+# 1.
+# 2.
+# 3.
+# 4.
+# 5.
+
+# TODO:
+# 1. Renovate error messages/headers 
+# 2.
+# 3.
+# 4.
+# 5.
+
+# -------------------------------------------------------------------
+
+# Config path
+### config_path = "bioPipe_config.ini"
+config_path = 'test_config.ini'
+
+# Config contents
+config_header = "# This is an automatically generated configuration file\n"
+section_name = 'BIOPIPE'
+trim_jar_path_key = 'trimJarPath'
+
+# Errors
+key_error = 'ERROR: INI Key error encountered'
---
+++ config/config.py
@@ -0,0 +1,88 @@
+# File: configBox.py
+# Author: Harrison Inocencio
+# Date: 07-18-18
+# Purpose: Contains the config class, a container for r/w to the config file
+#		   and storing it's parameters.
+
+# Notes:
+# 1.
+# 2.
+# 3.
+# 4.
+# 5.
+
+# TODO:
+# 1. 
+# 2.
+# 3.
+# 4.
+# 5.
+
+# -------------------------------------------------------------------
+
+import os
+import configparser
+import cargs
+
+# config class
+class config():
+	"""
+	Contains the parameters read from the config file, but can also
+	Write out it's own attributes into a new file. Attributes will
+	be set to 'None' if the initial read was unsuccessful, but this can
+	also be checked using the 'check_path' func
+	"""
+
+	trim_jar_path = None
+
+	# __init__ func
+	# By default, will automatically try to load ini file
+	def __init__(self):
+		try:
+			self.ini_read()
+		except FileNotFoundError:
+			pass	
+			
+	# __parse_ini func
+	# Parses init configuration file and sets attributes
+	def __parse_ini(self):
+		config = configparser.ConfigParser()
+		config.read(cargs.config_path)
+		self.trim_jar_path = config[cargs.section_name][(
+												cargs.trim_jar_path_key)]
+
+	# __check_path func
+	# Attempts to locate config file path
+	# If not found, raises a FileNotFound exception
+	def check_path(self):
+		if not os.path.isfile(cargs.config_path):
+			raise(FileNotFoundError)
+
+	# ini_read func
+	# Attempts to update its attributes from the ini file
+	# Will riase a FileNotFoundError if the file could not be located
+	# Will raise a KeyError if a the file experiences a key formatting 
+	# issue
+	def ini_read(self):
+		self.check_path()
+		try:
+			self.__parse_ini()
+		except KeyError:
+			raise(RuntimeError(cargs.key_error))
+
+	# config_write func
+	# The object will write out its current attributes into the config file
+	# Will overwrite current config file
+	def config_write(self):
+		config = configparser.ConfigParser()
+		config[cargs.section_name] = {}
+		config[cargs.section_name][cargs.trim_jar_path_key] = (
+														self.trim_jar_path)
+		with open(cargs.config_path, 'w') as fh:
+			fh.write(cargs.config_header+'\n')
+			config.write(fh)
+	
+	# tprint func
+	# Print out all attributes (for testing)
+	def tprint(self):
+		print(self.trim_jar_path)
